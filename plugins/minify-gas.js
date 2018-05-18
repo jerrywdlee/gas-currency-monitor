80474a4ad40c188422679f0dd26fa97c99c23781
Jerry Lee <lee@idoctor.co.jp>
2018-05-18 20:50:53 +0900

    update minify plugin

--- plugins/minify-gas.js
+++ plugins/minify-gas.js
@@ -4,7 +4,8 @@ function MinifyGas() {
 }
 
 function minify(source) {
-  var comment_reg = /\/\*.*?\*\/|\/\/.*/g;
+  var comment_reg = /\/\*.*?\*\//g;
+  var comment_reg2 = /\n\/\/.*/g
   var tab_reg = /\t/g;
   var space_reg = /(\n)\s+/g;
   var new_line_reg = /\n{2,}/g;
@@ -15,7 +16,8 @@ function minify(source) {
   .replace(tab_reg, '')
   .replace(space_reg, '$1')
   .replace(new_line_reg, '')
-  .replace(semicolon_reg, '$1$2');
+  .replace(semicolon_reg, '$1$2')
+  .replace(comment_reg2, '');
   return tmp_source
 }
 
